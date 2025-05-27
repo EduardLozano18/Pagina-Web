dfe4377a1c320a5d7e94f023fd3bfe44041c4e47
EduardLozano18 <eduard.loz2001@gmail.com>
2025-05-26 22:59:06 -0600

    Actualizacion:Carrito

--- js/main.js
+++ js/main.js
@@ -108,6 +108,10 @@ sr.reveal(`.about__img, .discount__data`,{origin: 'right'})
 /*Carrito de compras */
 const listProducts = document.querySelector('#trick');
 const listProduct = document.querySelector('#new');
+const contentProducts = document.querySelector('#contentProducts');
+const emptyCart = document.querySelector('#emptyCart');
+
+let productsArray = [];
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -116,17 +120,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+
+
 function EventListener(){
    listProducts.addEventListener('click', getDataElements);
    listProduct.addEventListener('click', getDataElements);
 
+   emptyCart.addEventListener('click', function(){
+    productsArray = [];
+    productHTML();
+    updateCarCount();
+    updatetotal();
+   })
+
+   const loadproduct = localStorage.getItem('products');
+   if (loadproduct){
+     productsArray = JSON.parse(loadproduct);
+     productHTML();
+     updateCarCount();
+     updatetotal();
+   }else{
+    productsArray = [];
+   }
 }
+
+function updateCarCount(){
+    const cartCount = document.querySelector('#cartCount');
+    cartCount.textContent = productsArray.length;
+}
+
+function updatetotal() {
+    const total = document.querySelector('#total');
+    let totalProduct = productsArray.reduce((total, product) => total + product.price * product.quantity, 0);
+    total.textContent = `$${totalProduct.toFixed(2)}`;
+}
+
 
 function getDataElements(event){
     if(event.target.classList.contains('bx-cart-alt')) {
         const elementHTML = event.target.parentElement.parentElement;
         selectData(elementHTML);
-    };
+    }
 }
 
 function selectData(product){
@@ -134,12 +168,111 @@ function selectData(product){
         img: product.querySelector('img').src,
         title: product.querySelector('h3').textContent,
         price: parseFloat(product.querySelector('.new__price , .trick__price').textContent.replace('$', '')),
-        id: product.querySelector('button[type="button"]').id
+        id: parseInt(product.querySelector('.button').dataset.id),
+        quantity:1
     }
 
-    console.log(productOBJ)
+    const exist = productsArray.some(product => product.id === productOBJ.id);
+    if (exist){
+        showAlert('El producto ya existe en el carrito', 'error');
+        return;
+    }
+    productsArray = [...productsArray, productOBJ];
+    // console.log(productsArray);
+    showAlert('El producto fue añadido al carrito', 'success');
+    productHTML();
+    updateCarCount();
+    updatetotal();
 }
 
-// function getDataElements(e){
-//     console.log(e.target.classList);
-// }
+function productHTML(){
+    cleanhtml();
+    productsArray.forEach(product =>{
+        const {img, title, price, quantity, id} = product;
+
+        const tr = document.createElement('tr');
+        const tdImg = document.createElement('td');
+        const prodImg = document.createElement('img');
+        prodImg.src = img;
+        prodImg.alt = 'image product'
+        tdImg.appendChild(prodImg);
+
+        const tdTitle = document.createElement('td');
+        const producttitle = document.createElement('p');
+        producttitle.textContent=title;
+        tdTitle.appendChild(producttitle);
+
+        const tdprice = document.createElement('td');
+        const productprice = document.createElement('p');
+        const newPrice = price * quantity;
+        productprice.textContent= `$${newPrice.toFixed(2)}`;
+        tdprice.appendChild(productprice);
+
+        const tdQuantity = document.createElement('td');
+        const productQuantity = document.createElement('input');
+        productQuantity.type='number';
+        productQuantity.min='1';
+        productQuantity.value=quantity;
+        productQuantity.dataset.id = id;
+        productQuantity.oninput = updatequantity;
+        tdQuantity.appendChild(productQuantity);
+
+        const tdDelete = document.createElement('td');
+        const productDelete = document.createElement('button');
+        productDelete.type = 'button';
+        productDelete.textContent = 'X';
+        productDelete.onclick = () => destroyprodct(id);
+        tdDelete.appendChild(productDelete);
+
+        tr.append(tdImg, tdTitle, tdprice, tdQuantity, tdDelete);
+        contentProducts.appendChild(tr);
+
+    })
+    savelocalsto();
+}
+
+function savelocalsto(){
+    localStorage.setItem('products', JSON.stringify(productsArray));
+
+}
+
+
+ function cleanhtml(){
+        contentProducts.innerHTML = '';
+    }
+
+
+    function updatequantity(event){
+    const newQuantity = parseInt(event.target.value, 10);
+    const idProd = parseInt(event.target.dataset.id, 10);
+
+    const product1 = productsArray.find(product => product.id === idProd);
+    if (product1 && newQuantity >0){
+        product1.quantity = newQuantity;
+    }
+    productHTML();
+    updatetotal();
+    savelocalsto();
+}
+
+function destroyprodct(idproduct){
+    productsArray = productsArray.filter(product => product.id !== idproduct);
+    showAlert('El producto fue eliminado del carrito', 'success');
+    productHTML();
+    updateCarCount();
+    updatetotal();
+    savelocalsto();
+}
+
+
+    function showAlert(message, type){
+        const nonrepetalert = document.querySelector('.alert');
+        if (nonrepetalert) nonrepetalert.remove();
+        const div = document.createElement('div');
+        div.classList.add('alert',type);
+        div.textContent = message;
+
+        document.body.appendChild(div);
+
+        setTimeout(() => div.remove(), 1000);
+    }
